4266c57a1d3f85de347899bae7e4d04465058c69
poikcue <poikcue@outlook.com>
2022-06-19 15:46:44 +0800

    Original Change

--- src/com/poikcue/MiAutoWhitelist/welcome.java
+++ src/com/poikcue/MiAutoWhitelist/welcome.java
@@ -15,21 +15,6 @@ import static org.bukkit.Bukkit.*;
 public class welcome implements Listener {
     @EventHandler
     public void welcome(MiraiGroupMemberJoinEvent e) throws IOException {
-        Yaml yml = new Yaml();
-        FileReader reader = new FileReader("plugins/MiraWhitelist/List.yml");
-        BufferedReader buffer = new BufferedReader(reader);
-        Map<String,Object> mapA = yml.load(buffer);
-        buffer.close();
-        reader.close();
-        if(mapA.get(e.getNewMemberID()) != null){
-            Map<String, Object> mapB = new HashMap<>();
-            FileWriter writer = new FileWriter("plugins/MiraWhitelist/List.yml");
-            BufferedWriter bufferB = new BufferedWriter(writer);
-            getScheduler().runTask(Main.getInstance(), () -> dispatchCommand(getConsoleSender(), "whitelist add " + mapB.get(e.getNewMemberID())));
-            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage(Main.getInstance().getConfig().getString("Message.WelcomeRejoinFriend"));
-        }
-        else{
-            MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage(Main.getInstance().getConfig().getString("Message.WelcomeFriend"));
-        }
+        MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).sendMessage(Main.getInstance().getConfig().getString("Message.WelcomeRejoinFriend"));
     }
 }
--- src/com/poikcue/MiAutoWhitelist/whitelist.java
+++ src/com/poikcue/MiAutoWhitelist/whitelist.java
@@ -19,48 +19,22 @@ import static org.bukkit.Bukkit.*;
 public class whitelist implements Listener {
     @EventHandler
     public void onMessageGet(MiraiGroupMessageEvent e) throws IOException {
-        if(Main.getInstance().getConfig().getString("Group.WorkInGroup").equals(e.getGroup())){
             if (e.getMessage().contains(Main.getInstance().getConfig().getString("Message.ReplaceFriendMessage"))) {
                 String name = e.getMessage().replace(Main.getInstance().getConfig().getString("Message.ReplaceFriendMessage"), "");
                 URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
                 if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                    Yaml yml = new Yaml();
-                    FileReader reader = new FileReader("plugins/MiraWhitelist/List.yml");
-                    BufferedReader buffer = new BufferedReader(reader);
-                    Map<String,Object> mapA = yml.load(buffer);
-                    buffer.close();
-                    reader.close();
-                    if (mapA.get(e.getSenderID()) == null) {
-                        e.sendMessage(Main.getInstance().getConfig().getString("Message.PremiumMinecraftAccountMessage"));
-                        Map<String, Object> mapB = new HashMap<>();
-                        mapB.put(e.getSenderName(), name);
-                        FileWriter writer = new FileWriter("plugins/MiraWhitelist/List.yml", true);
-                        BufferedWriter bufferW = new BufferedWriter(writer);
-                        bufferW.newLine();
-                        yml.dump(mapB, bufferW);
-                        buffer.close();
-                        writer.close();
-                        getScheduler().runTask(Main.getInstance(), () -> dispatchCommand(getConsoleSender(), "whitelist add " + name));
-                    }
-                    else{
-                        e.sendMessage(Main.getInstance().getConfig().getString("Message.AlreadySignedUp"));
-                    }
+                    e.sendMessage(Main.getInstance().getConfig().getString("Message.PremiumMinecraftAccountMessage"));
+                    getScheduler().runTask(Main.getInstance(), () -> dispatchCommand(getConsoleSender(), "whitelist add " + name));
+                    //正版玩家
                 } else if (connection.getResponseCode() == HttpURLConnection.HTTP_NO_CONTENT) {
                     e.sendMessage(Main.getInstance().getConfig().getString("Message.CrackedMinecraftAccountAlert"));
+                    //正常访问，但是离线玩家
                 } else {
                     e.sendMessage(Main.getInstance().getConfig().getString("Message.UrlError"));
+                    //出现问题，访问异常
                 }
             }
         }
     }
-}
-
-/*
-History data:
-File config = new File("plugins/MiraiWhitelist/config.yml");
-                    YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(config);
-                    List<String> list = yamlConfiguration.getStringList("List");
-                    String whitelist = Main.getInstance().getConfig().getString("List");
-*/
